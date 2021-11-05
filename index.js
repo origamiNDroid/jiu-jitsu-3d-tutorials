79e050a8d41bfca677b341a2ed9ed9cff721d65b
Pryan3777 <Pryan3777@gmail.com>
2021-11-05 13:54:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.use((err, req, res, next) => {
   next();
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
