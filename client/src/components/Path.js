0739cdf35c421e360d428642c4829d6d82f62b66
JaredNeumann <56045454+JaredNeumann@users.noreply.github.com>
2022-04-11 11:41:28 -0400

    Revert "Front end merge"

--- client/src/components/Path.js
+++
@@ -1,9 +0,0 @@
-const app_name = 'jiu-jitsu-3d-tutorials'
-exports.buildPath =
-function buildPath(route)
-{
-    if (process.env.NODE_ENV === 'production')
-        return 'https://' + app_name + 'herokuapp.com/' + route;
-    else
-        return 'http://localhost:5000/' + route;
-}
